0b352e813522f1e2ee4e58e3ad74a55e66337e8f
Shashank Ananthoju <ananthoj@sheridancollege.ca>
2020-03-28 18:12:04 -0400

    HTML completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  header = 'Exercise4';
+  header = 'Exercise 4';
+  image1: string;
+  alt: string;
+  label: string;
+
+
+  constructor() {
+
+  }
+
+  dog() {
+    this.image1 = 'assets/images/dog.PNG';
+    this.alt = 'dog_img';
+    this.label = 'Dog';
+  }
+
+    cat() {
+      this.image1 = 'assets/images/cat.PNG';
+      this.alt = 'cat_img';
+      this.label = 'Cat'
+    }
+
+    rabbit() {
+      this.image1 = 'assets/images/rabbit.jpg';
+      this.alt = 'rabbit_img';
+      this.label = 'Rabbit';
+    }
 }
